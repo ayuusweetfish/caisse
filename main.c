68774b304e27b74bb5dcaeb8e6e26b6c7cc46989
Shiqing <shiqing-thu18@yandex.com>
2022-09-23 23:57:14 +0800

    Starting out

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+  FILE *f = fopen("index.html", "r");
+  fseek(f, 0, SEEK_END);
+  size_t len = (size_t)ftell(f);
+  fseek(f, 0, SEEK_SET);
+  char *buf = (char *)malloc(len);
+  fread(buf, len, 1, f);
+  fclose(f);
+  for (size_t i = 0; i < len; i++) putchar(buf[i]);
+
+  DIR *dir = opendir("site");
+  struct dirent *ent;
+  while ((ent = readdir(dir)) != NULL) {
+    puts(ent->d_name);
+  }
+
   return 0;
 }
