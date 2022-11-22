1c8ac06109134bc064d9159270db08200413048d
Ayu <backyard@ayu.land>
2022-11-22 22:53:50 +0800

    Render templates with nested contexts

--- main.lua
+++ main.lua
@@ -1,6 +1,27 @@
+-- Item format:
+-- string
+-- function
+-- {type = string, expr = string/function, span = number}
 local loadtemplate = function (s)
   local items = {}
   local last, cur = 0, 1
+  local levelbegin = {}
+  local loadfn = function (expr)
+    local fn = load('return ' .. expr, expr, 't')
+    if fn == nil then
+      fn = load(expr, expr, 't')
+      if fn == nil then error('Statement "' .. expr:sub(2) .. '" is invalid') end
+    end
+    return function (locals)
+      setmetatable(_ENV, {
+        __index = locals,
+        __newindex = function (table, key, value) locals[key] = value end,
+      })
+      local ret = fn()
+      setmetatable(_ENV, nil)
+      return ret
+    end
+  end
   while cur <= #s do
     if s:sub(cur, cur + 1) == '{{' then
       -- New text entry
@@ -16,16 +37,23 @@ local loadtemplate = function (s)
       -- Trim substring
       local startpos = s:find('[^%s]', cur + obrkts)
       local expr = s:sub(startpos, endpos - 1)
-      -- Detect type
-      local fn = load(
-        'setmetatable(_ENV, {__index=...}) return ' .. expr,
-        expr, 't')
-      if fn ~= nil then
-        items[#items + 1] = function (locals)
-          return fn(locals)
+      if expr:sub(1, 1) == '@' then
+        if expr:match('^@%s*end%s*$') then
+          -- Move up one level
+          items[levelbegin[#levelbegin]].span = #items
+          levelbegin[#levelbegin] = nil
+        else
+          -- Nested scope
+          local fn = loadfn(expr:sub(2))
+          items[#items + 1] = {type = 'scope', expr = fn, span = 0}
+          levelbegin[#levelbegin + 1] = #items
         end
+      elseif expr:sub(-1) == '=' then
+        -- Block
+        items[#items + 1] = {type = 'block', expr = expr:sub(1, -2), span = 0}
       else
-        items[#items + 1] = expr
+        -- Detect type
+        items[#items + 1] = loadfn(expr)
       end
       last = endext
       cur = endext + 1
@@ -39,15 +67,62 @@ local loadtemplate = function (s)
   return items
 end
 
-local render = function (template, locals)
-  local i = 1
-  while i <= #template do
+local function render(template, locals, outputs, rangestart, rangeend)
+  outputs = outputs or {}
+  rangestart = rangestart or 1
+  rangeend = rangeend or #template
+  --print('render', rangestart, rangeend, require('inspect')(locals))
+  local i = rangestart
+  while i <= rangeend do
     local item = template[i]
-    if type(item) == 'function' then
-      print(item({title = 'a', intro = 'b', contents = 'c'}))
-    else print(item)
+    --[[if type(item) == 'function' then
+      print(i, item({title = 'a', intro = 'b', contents = 'c'}))
+    elseif type(item) == 'table' then
+      print(i, item.type, item.expr, item.span)
+    else print(i, item)
+    end]]
+    if type(item) == 'string' then
+      --print('raw text', (item:sub(1, 20):gsub('\n', '.')))
+      outputs[#outputs + 1] = item
+    elseif type(item) == 'function' then
+      local value = item(locals)
+      if value ~= nil then outputs[#outputs + 1] = value end
+    elseif type(item) == 'table' then
+      if item.type == 'scope' then
+        local ctx = item.expr(locals)
+        if type(ctx) == 'boolean' or ctx == nil then
+          if not ctx then i = item.span end
+        elseif type(ctx) == 'table' then
+          local unpackctx = function (ctx)
+            local stash = {}
+            for k, v in pairs(ctx) do
+              stash[k] = locals[k]
+              locals[k] = v
+            end
+            render(template, locals, outputs, i + 1, item.span)
+            for k, v in pairs(ctx) do
+              locals[k] = stash[k]
+            end
+          end
+          if #ctx > 0 then
+            -- Loop
+            for j = 1, #ctx do unpackctx(ctx[j]) end
+          else
+            -- Unpack
+            unpackctx(ctx)
+          end
+          i = item.span
+        else
+          error('Expression should either be a boolean or a table value')
+        end
+      elseif item.ty == 'block' then
+        -- TODO
+      end
     end
     i = i + 1
+  end
+  if rangestart == 1 and rangeend == #template then
+    print('render!', table.concat(outputs, ''))
   end
 end
 
