4ec4cdc61e83b2c8ee7fb9befe47029ea5fb7f67
Ayu <backyard@ayu.land>
2022-11-28 23:07:54 +0800

    Increase to 8 season icons

--- misc/little-icons/build.sh
+++ misc/little-icons/build.sh
@@ -1,6 +1,6 @@
 pyftsubset NotoEmoji-Regular.ttf \
   --output-file=seasons-NotoEmoji.ttf \
-  --unicodes=1f340,1f333,1f343,2744
+  --unicodes=1f340,1f338,1f333,2600,1f33e,1f343,2744,1f9ca
 mv seasons-NotoEmoji.ttf little-icons.ttf
 woff2_compress little-icons.ttf
 mv little-icons.woff2 ../../content
