6f1e984fab069fa9e9ea103d5ae81de1bafc2f29
Ayu <backyard@ayu.land>
2022-12-02 21:34:20 +0800

    Add file list markup

--- misc/little-icons/build.sh
+++ misc/little-icons/build.sh
@@ -4,6 +4,8 @@ UNICODES=1f340,1f338,1f333,2600,1f33e,1f343,2744,1f9ca
 UNICODES=${UNICODES},1f310,1f319,26aa
 # External link, star
 UNICODES=${UNICODES},1fa90,1f31f
+# File types
+UNICODES=${UNICODES},1f4e6,1f3a7,1f3b6,1f3bc,1f5bc,1f39e,1f4c3,1f47e
 pyftsubset NotoEmoji-Regular.ttf \
   --output-file=seasons-NotoEmoji.ttf \
   --unicodes=${UNICODES}
