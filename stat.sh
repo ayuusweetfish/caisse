0e982ac0a40fcb2c8633c5b8e47fcfa3dfbe2139
Ayu <backyard@ayu.land>
2023-01-03 21:46:13 +0800

    Ignore page.txt files in filestat database

--- stat.sh
+++ stat.sh
@@ -3,4 +3,5 @@ find ../../content -type f \
   -not -path "../../content/fonts/*" \
   -not -path "../../content/fonts-zh/*" \
   -not -path "../../content/vendor/*" \
+  -not -name "page.txt" \
   | lua build.lua ../../content/
