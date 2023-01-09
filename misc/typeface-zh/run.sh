4b69451845ca66bf734ab6a24eb8c4001e7c9f2f
Shiqing <shiqing-thu18@yandex.com>
2023-01-10 00:58:08 +0800

    Add backyard (content inside a separate repository)

--- misc/typeface-zh/run.sh
+++ misc/typeface-zh/run.sh
@@ -1,6 +1,6 @@
 g++ solve.cc -std=c++11 -O2
 if [[ "$FULL" == "1" ]]; then
-  find ../../build -name "index.zh.html" | awk -F/ '{ print $0; print $(NF-1) }' | ./a.out > common.txt
+  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | awk -F/ '{ print $0; print $(NF-1) }' | ./a.out > common.txt
 fi
 find ../../build -name "index.zh.html" | awk -F/ '{ print $0; print $(NF-1) }' | INC=1 ./a.out > stray.txt
 lua process.lua
