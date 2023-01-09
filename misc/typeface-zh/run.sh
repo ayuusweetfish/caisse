eb395c205579f3e9929f2d7914f555e3b12c0d89
Ayu <backyard@ayu.land>
2023-01-10 02:12:39 +0800

    Consider backyard (item names with slash) in typeface subsetting script

--- misc/typeface-zh/run.sh
+++ misc/typeface-zh/run.sh
@@ -1,8 +1,8 @@
 g++ solve.cc -std=c++11 -O2
 if [[ "$FULL" == "1" ]]; then
-  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | awk -F/ '{ print $0; print $(NF-1) }' | ./a.out > common.txt
+  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | ./a.out > common.txt
 fi
-find ../../build -name "index.zh.html" | awk -F/ '{ print $0; print $(NF-1) }' | INC=1 ./a.out > stray.txt
+find ../../build -name "index.zh.html" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | INC=1 ./a.out > stray.txt
 lua process.lua
 for i in AaKaiSong.*.ttf; do woff2_compress $i; rm $i; done
 rm ../../content/fonts-zh/AaKaiSong.*
