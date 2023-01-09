066c75f277a3441c9e91ab72f728d40583b290a6
Shiqing <shiqing-thu18@yandex.com>
2023-01-10 02:12:39 +0800

    Consider backyard (item names with slash) in typeface subsetting script

--- misc/typeface-zh/solve.cc
+++ misc/typeface-zh/solve.cc
@@ -1,7 +1,7 @@
 #if 0 // due to /* inside comments
   g++ solve.cc -std=c++11 -O2
-  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | awk -F/ '{ print $0; print $(NF-1) }' | ./a.out > common.txt
-  find ../../build -name "index.zh.html" | awk -F/ '{ print $0; print $(NF-1) }' | INC=1 ./a.out > stray.txt
+  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | ./a.out > common.txt
+  find ../../build -name "index.zh.html" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | INC=1 ./a.out > stray.txt
 #endif
 
 #include <cstdint>    // uint64_t
