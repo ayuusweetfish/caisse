abfd977453557ba1a5c66499c048e45a907ab8f2
Ayu <claire@ayu.land>
2023-01-14 21:15:43 +0800

    timeline: Finish Douban PoC

--- misc/timeline/test_douban/1.js
+++ misc/timeline/test_douban/1.js
@@ -26,6 +26,25 @@ const recurseMod = (el) => {
   }
 }
 
+/*
+    cyrb53 (c) 2018 bryc (github.com/bryc)
+    A fast and simple hash function with decent collision resistance.
+    Largely inspired by MurmurHash2/3, but with a focus on speed/simplicity.
+    Public domain. Attribution appreciated.
+*/
+const hash_cyrb_hex64 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
+  }
+  h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909)
+  h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909)
+  return (h2>>>0).toString(16).padStart(8, '0') +
+         (h1>>>0).toString(16).padStart(8, '0')
+}
+
 const imagesToDownload = []
 
 for (const wrapper of els) {
@@ -35,10 +54,7 @@ for (const wrapper of els) {
   let html = wrapper.outerHTML
   html = html.replace(/https?:\/\/img(.+)\.doubanio\.com([A-Za-z0-9-_%\/.@]+)/g,
     (s) => {
-      let hash1 = 0, hash2 = 0
-      const hashStr =
-        hash1.toString(16).padStart(8, '0') +
-        hash2.toString(16).padStart(8, '0')
+      const hashStr = hash_cyrb_hex64(s)
       imagesToDownload.push([s, hashStr])
       const ext = s.match(/\.([^.]+)$/)[1]
       return `/img-douban/${hashStr}.${ext}`
