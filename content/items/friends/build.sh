b5ce4510328137047133f2c65c573f47bda56df9
Ayu <claire@ayu.land>
2023-02-01 15:12:17 +0800

    Add XXIIVV webring

--- content/items/friends/build.sh
+++ content/items/friends/build.sh
@@ -1,8 +1,14 @@
+specified="$*"
+
 # https://superuser.com/q/524793
 process() {
   name=$1
   tint=$2
-  convert $name.png -colorspace gray -negate -evaluate Multiply 0.2 -alpha copy \
+  if [[ "$specified" != "" ]] && [[ "$specified" != *"$name"* ]]; then
+    return
+  fi
+  echo Processing $name
+  convert $name.png -scale x300 -colorspace gray -negate -evaluate Multiply 0.2 -alpha copy \
     -channel rgb -set colorspace sRGB -fx $tint $name-bg.png
 }
 
@@ -10,4 +16,4 @@ process alicez '#0047ab'
 process chiyuru '#5a7894'
 process pieris '#58b2dc'
 process xhhdd '#333d42'
-process webring '#010101'
+process xxiivv-webring '#010101'
