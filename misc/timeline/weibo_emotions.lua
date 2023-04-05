b04964d4d1ccc93cb451106205e60bf7ce2fd358
Ayu <claire@ayu.land>
2023-04-05 16:29:16 +0800

    timeline: Add Weibo emotion [浮云]

--- misc/timeline/weibo_emotions.lua
+++ misc/timeline/weibo_emotions.lua
@@ -49,6 +49,7 @@ local list = {
 ['锦鲤附体'] = 'https://face.t.sinajs.cn/t4/appstyle/expression/ext/normal/e3/2022_cometrue_org.png',
 ['鞭炮声声'] = 'https://face.t.sinajs.cn/t4/appstyle/expression/ext/normal/7f/2023_firecrackers_org.png',
 ['抓狂'] = 'https://face.t.sinajs.cn/t4/appstyle/expression/ext/normal/17/2018new_zhuakuang_org.png',
+['浮云'] = 'https://face.t.sinajs.cn/t4/appstyle/expression/ext/normal/61/2018new_yunduo_org.png',
 }
 
 local download = select(1, ...) == 'download'
