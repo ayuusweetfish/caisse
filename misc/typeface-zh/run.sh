dc1d9d945f1b00bef576d1cf5f96346dcd68511b
Ayu <claire@ayu.land>
2023-07-04 19:28:05 +0800

    typeface-zh: Separate subsets for language versions

--- misc/typeface-zh/run.sh
+++ misc/typeface-zh/run.sh
@@ -6,9 +6,9 @@ fi
 
 g++ solve.cc -std=c++11 -O2
 if [[ "$FULL" == "1" ]]; then
-  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | ./a.out > common.txt
+  find ../../build -name "index.*.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+\.([a-z]+)\.html)\n/\1\n\2.\3\n/g' | ./a.out > common.txt
 fi
-find ../../build -name "index.zh.html" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | INC=1 ./a.out > stray.txt
+find ../../build -name "index.*.html" | perl -pe 's/(.+build\/(.+)\/[^\/]+\.([a-z]+)\.html)\n/\1\n\2.\3\n/g' | INC=1 ./a.out > stray.txt
 
 exists() { [[ -f $1 ]]; }
 exists ../../content/fonts-zh/AaKaiSong.*.woff2 && mv ../../content/fonts-zh/AaKaiSong.*.woff2 .
--- misc/typeface-zh/solve.cc
+++ misc/typeface-zh/solve.cc
@@ -1,9 +1,3 @@
-#if 0 // due to /* inside comments
-  g++ solve.cc -std=c++11 -O2
-  find ../../build -name "index.zh.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | ./a.out > common.txt
-  find ../../build -name "index.zh.html" | perl -pe 's/(.+build\/(.+)\/[^\/]+)/\1\2\n/g' | INC=1 ./a.out > stray.txt
-#endif
-
 #include <cstdint>    // uint64_t
 #include <cstdio>     // fgets, fopen, fgetc
 #include <cstring>    // memset, memmove, strlen
