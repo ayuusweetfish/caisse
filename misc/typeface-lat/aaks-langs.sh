5dc52d5030ea6d9a1511f23064ca4ab31dea1967
Ayu <claire@ayu.land>
2026-02-16 16:18:36 +0800

    Check in font conversion script

---
+++ misc/typeface-lat/aaks-langs.sh
@@ -0,0 +1,5 @@
+fonttools subset ../../misc/typeface-zh/AaKaiSong2WanZi2.ttf --text="汉语英语" --output-file=/tmp/1.ttf
+sfnt2woff /tmp/1.ttf
+woff2_compress /tmp/1.ttf
+for j in woff woff2; do mv /tmp/1.$j AaKaiSong-langs.$j; done
+rm /tmp/1.ttf
