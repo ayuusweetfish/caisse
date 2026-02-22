aa8e8462c909fbb243caa12c6e41483e5291bce7
Ayu <claire@ayu.land>
2026-02-22 12:26:56 +0800

    Avoid recompiling typeface subset solver every time

--- misc/typeface-zh/run.sh
+++ misc/typeface-zh/run.sh
@@ -1,11 +1,15 @@
 if [[ "$FULL_DEBUG" == "1" ]]; then
-  woff2_compress AaKaiSong2WanZi2.ttf
-  mv AaKaiSong2WanZi2.woff2 ../../content/fonts-zh/AaKaiSong2-full.woff2
+  woff2_compress AaKaiSong2WanZi2_mapped.ttf
+  mv AaKaiSong2WanZi2_mapped.woff2 ../../content/fonts-zh/AaKaiSong2-full.woff2
   exit
 fi
 
-SOLVE_EXEC=$(mktemp)
-g++ solve.cc -std=c++11 -O2 -o ${SOLVE_EXEC}
+SOLVE_EXEC=/tmp/caisse-typeface-zh-solve
+if [[ ! -e "${SOLVE_EXEC}" ]]; then
+  echo "Compiling solver"
+  g++ solve.cc -std=c++11 -O2 -o ${SOLVE_EXEC}
+fi
+
 if [[ "$FULL" == "1" ]]; then
   find ../../build -name "index.*.html" -not -path "../../build/backyard/*" | perl -pe 's/(.+build\/(.+)\/[^\/]+\.([a-z]+)\.html)\n/\1\n\2.\3\n/g' | ${SOLVE_EXEC} > common.txt
 fi
@@ -15,4 +19,3 @@ exists() { [[ -f $1 ]]; }
 exists ../../content/fonts-zh/AaKaiSong.*.woff2 && mv ../../content/fonts-zh/AaKaiSong.*.woff2 .
 lua process.lua
 test -n "$(find . -maxdepth 1 -name 'AaKaiSong.*.woff2' -print -quit)" && rm AaKaiSong.*.woff2
-rm ${SOLVE_EXEC}
